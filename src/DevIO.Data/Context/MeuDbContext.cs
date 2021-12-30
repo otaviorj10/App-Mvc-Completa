30c479d0d16cebd4d1950e10ecd4385de91fa52e
Otavio <luiz.nascimento@innolevels.com.br>
2021-12-30 07:55:57 -0300

    feito a criacao da classe context herdando de dbcontext pronta pra usar fluent API , para realizar mapeamento das entidades

---
+++ src/DevIO.Data/Context/MeuDbContext.cs
@@ -0,0 +1,31 @@
+﻿using AppBasicaMvc.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevIO.Data.Context
+{
+    class MeuDbContext :DbContext
+    {
+        public MeuDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Fornecedor> Fornecedores { get; set; }
+        public DbSet<Endereco> Enderecos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+
+}
